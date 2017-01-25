5facb6ab1598d1b9d7c46d78eec774f605aa75a5
Yul <dbfgml1006@gmail.com>
2017-01-25 14:28:11 +0900

    asdf

--- src/main/java/spring/domain/QuestionRepository.java
+++ src/main/java/spring/domain/QuestionRepository.java
@@ -3,6 +3,5 @@ package spring.domain;
 import org.springframework.data.repository.CrudRepository;
 
 public interface QuestionRepository extends CrudRepository<Question, Long> {
-	Question findByUserId(User writer);
-	Question findByUserId(long id);
+//sadfdsfdsfsas
 }
